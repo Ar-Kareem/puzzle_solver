b0117f926cd3d4fde7e9cb9e261971b13b1a4246
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-11-06 21:42:23 -0500

    area 51 refactors

--- src/puzzle_solver/puzzles/area_51/area_51.py
+++ src/puzzle_solver/puzzles/area_51/area_51.py
@@ -16,11 +16,11 @@ def get_ray(pos: Pos, V: int, H: int, direction: Direction) -> list[Pos]:
         out.append(pos)
     return out
 
-def get_eq_var(model: cp_model.CpModel, a: cp_model.IntVar, b: cp_model.IntVar) -> cp_model.IntVar:
+def get_neq_var(model: cp_model.CpModel, a: cp_model.IntVar, b: cp_model.IntVar) -> cp_model.IntVar:
     eq_var = model.NewBoolVar(f'{a}:{b}:eq')
     model.Add(a == b).OnlyEnforceIf(eq_var)
     model.Add(a != b).OnlyEnforceIf(eq_var.Not())
-    return eq_var
+    return eq_var.Not()
 
 def enforce_groups_opposite_when(model: cp_model.CpModel, group_a: list[cp_model.IntVar], group_b: list[cp_model.IntVar]):
     gate = model.NewBoolVar(f"gate_opposite_when_b[{group_a}]:{group_b}")
@@ -46,7 +46,6 @@ class Board:
         self.model = cp_model.CpModel()
         self.b: dict[Pos, cp_model.IntVar] = {}
         self.w: dict[Pos, cp_model.IntVar] = {}
-        self.debug = {}
 
         self.create_vars()
         self.add_all_constraints()
@@ -61,27 +60,27 @@ class Board:
             k = str(get_char(self.board, pos)).strip()
             if not k:
                 continue
-            if k[0] == 'O':
+            if k[0] == 'O':  # O{number} is a range clue
                 k = int(k[1:])
                 self.range_clues(pos, k)
-            elif k.isdecimal():
+            elif k.isdecimal():  # {number} is a number clue
                 sum_white_neighbors = lxp.Sum([self.w[p] for p in get_neighbors4(pos, self.V, self.H)])
                 sum_black_neighbors = 4 - sum_white_neighbors  # cells outside of border are black by default
                 self.model.Add(sum_white_neighbors == int(k)).OnlyEnforceIf(self.b[pos])
                 self.model.Add(sum_black_neighbors == int(k)).OnlyEnforceIf(self.w[pos])
-            elif k == 'A':  # aliens must be inside fence
+            elif k == 'A':  # A is alien; must be inside fence
                 self.model.Add(self.w[pos] == 1)
-            elif k == 'C':  # cactus must be outside fence
+            elif k == 'C':  # C is cactus; must be outside fence
                 self.model.Add(self.b[pos] == 1)
-        for pos, color in self.dots.items():
+        for pos, color in self.dots.items():  # this is the most complex part of the puzzle
+            assert color in ['W', 'B'], f'Invalid color: {color}'
             if color == 'W':
                 self.add_white_dot_constraints(pos)
             elif color == 'B':
                 self.add_black_dot_constraints(pos)
-                pass
-            else:
-                raise ValueError(f'Invalid color: {color}')
+        # inside the fence, all cells must be connected
         force_connected_component(self.model, self.w)
+        # outside the fence, all cells must be connected, + outside border is considered black
         def is_outside_neighbor(p1: Pos, p2: Pos) -> bool:
             if abs(p1.x - p2.x) + abs(p1.y - p2.y) == 1:  # manhattan distance is 1
                 return True
@@ -127,70 +126,35 @@ class Board:
         return tl_v, tr_v, bl_v, br_v, tl_u_v, tl_l_v, tr_u_v, tr_r_v, bl_l_v, bl_d_v, br_d_v, br_r_v
 
     def add_white_dot_constraints(self, pos: Pos):
-        # either  1x2 X over 1x2 ~X (horizontal variant)
-        # or      2x1 X next to 2x1 ~X (vertical variant)
+        # for the main 2x2 block, either two horizontal 1x2 rectangles on top of each other or two vertical 1x2 rectangles next to each other
         tl, tr, bl, br, tl_u, tl_l, tr_u, tr_r, bl_l, bl_d, br_d, br_r = self.get_2_by_2_block_vars(pos)
-        # 8 possibilities:
-        # . X X .
-        # X Y Y X
-        # X Y Y X
-        # . X X .
         # if the horizontal variant, then at least one of the X's on the left/right must be different
-        # if the vertical variant, then at least one of the X's on the top/bottom must be different
-
         # horizontal variant will need at least one of these to be active
-        tl_l_different = get_eq_var(self.model, tl, tl_l).Not()
-        tr_r_different = get_eq_var(self.model, tr, tr_r).Not()
-        bl_l_different = get_eq_var(self.model, bl, bl_l).Not()
-        br_r_different = get_eq_var(self.model, br, br_r).Not()
+        horiz_different = get_neq_var(self.model, tl, tl_l) + get_neq_var(self.model, tr, tr_r) + get_neq_var(self.model, bl, bl_l) + get_neq_var(self.model, br, br_r)
+        # if the vertical variant, then at least one of the X's on the top/bottom must be different
         # vertical variant will need at least one of these to be active
-        tl_u_different = get_eq_var(self.model, tl, tl_u).Not()
-        tr_u_different = get_eq_var(self.model, tr, tr_u).Not()
-        bl_d_different = get_eq_var(self.model, bl, bl_d).Not()
-        br_d_different = get_eq_var(self.model, br, br_d).Not()
-
-        horiz_variant_gate = self.model.NewBoolVar(f"horiz_variant_gate_w[{pos}]")
-        self.model.Add(tl == tr).OnlyEnforceIf(horiz_variant_gate)
-        self.model.Add(bl == br).OnlyEnforceIf(horiz_variant_gate)
-        self.model.Add(tl != bl).OnlyEnforceIf(horiz_variant_gate)
-        self.model.Add(tr != br).OnlyEnforceIf(horiz_variant_gate)
-        self.model.Add(tl_l_different + tr_r_different + bl_l_different + br_r_different >= 1).OnlyEnforceIf(horiz_variant_gate)
-        vert_variant_gate = self.model.NewBoolVar(f"vert_variant_gate_w[{pos}]")
-        self.model.Add(tl == bl).OnlyEnforceIf(vert_variant_gate)
-        self.model.Add(tr == br).OnlyEnforceIf(vert_variant_gate)
-        self.model.Add(tl != tr).OnlyEnforceIf(vert_variant_gate)
-        self.model.Add(bl != br).OnlyEnforceIf(vert_variant_gate)
-        self.model.Add(tl_u_different + tr_u_different + bl_d_different + br_d_different >= 1).OnlyEnforceIf(vert_variant_gate)
+        vert_different = get_neq_var(self.model, tl, tl_u) + get_neq_var(self.model, tr, tr_u) + get_neq_var(self.model, bl, bl_d) + get_neq_var(self.model, br, br_d)
+        horiz_variant_gate = enforce_groups_opposite_when(self.model, [tl, tr], [bl, br])
+        self.model.Add(horiz_different >= 1).OnlyEnforceIf(horiz_variant_gate)
+        vert_variant_gate = enforce_groups_opposite_when(self.model, [tl, bl], [tr, br])
+        self.model.Add(vert_different >= 1).OnlyEnforceIf(vert_variant_gate)
         self.model.AddBoolOr([horiz_variant_gate, vert_variant_gate])
-        self.debug[(pos, 'horiz_variant_gate')] = horiz_variant_gate
-        self.debug[(pos, 'vert_variant_gate')] = vert_variant_gate
 
     def add_black_dot_constraints(self, pos: Pos):
         # in the 2x2 block, one block is X and the other 3 are ~X
         tl, tr, bl, br, tl_u, tl_l, tr_u, tr_r, bl_l, bl_d, br_d, br_r = self.get_2_by_2_block_vars(pos)
         # that one the is X must also have it's 2 corresponding outward neighbors also be X
-        # V1: block tl is the one that is different from the other 3
-        gate_1 = enforce_groups_opposite_when(self.model, [tl, tl_u, tl_l], [tr, bl, br, bl_l, tr_u])
-        # V2: block tr is the one that is different from the other 3
-        gate_2 = enforce_groups_opposite_when(self.model, [tr, tr_u, tr_r], [tl, bl, br, tl_u, br_r])
-        # V3: block bl is the one that is different from the other 3
-        gate_3 = enforce_groups_opposite_when(self.model, [bl, bl_l, bl_d], [tl, tr, br, tl_l, br_d])
-        # V4: block br is the one that is different from the other 3
-        gate_4 = enforce_groups_opposite_when(self.model, [br, br_d, br_r], [tl, tr, bl, bl_d, tr_r])
-        self.debug[(pos, 'gate_1')] = gate_1
-        self.debug[(pos, 'gate_2')] = gate_2
-        self.debug[(pos, 'gate_3')] = gate_3
-        self.debug[(pos, 'gate_4')] = gate_4
+        gate_1 = enforce_groups_opposite_when(self.model, [tl, tl_u, tl_l], [tr, bl, br, bl_l, tr_u])  # V1: block tl is the one that is different from the other 3
+        gate_2 = enforce_groups_opposite_when(self.model, [tr, tr_u, tr_r], [tl, bl, br, tl_u, br_r])  # V2: block tr is the one that is different from the other 3
+        gate_3 = enforce_groups_opposite_when(self.model, [bl, bl_l, bl_d], [tl, tr, br, tl_l, br_d])  # V3: block bl is the one that is different from the other 3
+        gate_4 = enforce_groups_opposite_when(self.model, [br, br_d, br_r], [tl, tr, bl, bl_d, tr_r])  # V4: block br is the one that is different from the other 3
         self.model.AddBoolOr([gate_1, gate_2, gate_3, gate_4])
 
     def solve_and_print(self, verbose: bool = True):
         def board_to_solution(board: Board, solver: cp_model.CpSolverSolutionCallback) -> SingleSolution:
-            for k, v in board.debug.items():
-                print(f'{k}: {solver.Value(v)}')
             return SingleSolution(assignment={pos: solver.Value(board.w[pos]) for pos in get_all_pos(board.V, board.H)})
         def callback(single_res: SingleSolution):
             print("Solution:")
-            print(np.array([[single_res.assignment[get_pos(x=c, y=r)] for c in range(self.H)] for r in range(self.V)]))
             print(combined_function(self.V, self.H,
                 cell_flags=id_board_to_wall_fn(np.array([[single_res.assignment[get_pos(x=c, y=r)] for c in range(self.H)] for r in range(self.V)]), border_is_wall=False, border_is=1),
                 center_char=lambda r, c: self.board[r, c].strip(),
--- tests/test_area_51.py
+++ tests/test_area_51.py
@@ -30,27 +30,21 @@ def test_ground():
     solutions = binst.solve_and_print()
     assert len(solutions) == 1, f'unique solutions != 1, == {len(solutions)}'
     solution = solutions[0].assignment
-
-
-def test_toy():
-    board = np.array([
-        ['  ', '  ', '  ', '  '],
-        ['  ', '  ', '  ', '  '],
-        ['  ', '  ', '  ', '  '],
-        ['  ', '  ', '  ', '  '],
-        ['  ', '  ', '  ', '  '],
-    ])
-    dots = {
-        get_pos(x=2, y=2): 'B', 
-        get_pos(x=1, y=2): 'W', 
-        get_pos(x=0, y=2): 'W', 
-    }
-    binst = solver.Board(board=board, dots=dots)
-    solutions = binst.solve_and_print()
-    assert len(solutions) == 1, f'unique solutions != 1, == {len(solutions)}'
-    solution = solutions[0].assignment
-
+    # print('ground = np.' + repr(np.array([[solution.get(get_pos(x=x, y=y), ' ') for x in range(board.shape[1])] for y in range(board.shape[0])]).astype(str)))
+    ground = np.array([['1', '1', '1', '1', '0', '1', '1', '1'],
+        ['1', '0', '0', '1', '0', '1', '1', '1'],
+        ['1', '0', '1', '1', '1', '1', '0', '0'],
+        ['0', '0', '0', '0', '0', '1', '1', '1'],
+        ['1', '0', '1', '1', '1', '1', '0', '0'],
+        ['1', '1', '1', '1', '0', '1', '1', '1'],
+        ['0', '0', '0', '0', '0', '1', '0', '0'],
+        ['1', '1', '0', '1', '1', '1', '1', '1'],
+        ['0', '1', '1', '1', '1', '0', '0', '0'],
+        ['1', '1', '1', '0', '1', '1', '1', '1']], dtype='<U11')
+    ground_assignment = {get_pos(x=x, y=y): ground[y][x] for x in range(ground.shape[1]) for y in range(ground.shape[0])}
+    assert set(solution.keys()) == set(ground_assignment.keys()), f'solution keys != ground assignment keys, {set(solution.keys()) ^ set(ground_assignment.keys())} \n\n\n{solution} \n\n\n{ground_assignment}'
+    for pos in solution.keys():
+        assert str(solution[pos]) == ground_assignment[pos], f'solution[{pos}] != ground_assignment[{pos}], {solution[pos]} != {ground_assignment[pos]}'
 
 if __name__ == "__main__":
     test_ground()
-    # test_toy()
