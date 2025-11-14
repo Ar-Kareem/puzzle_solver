791f01e4763a1c43e83bfb9a6bc0fce2c07d3bff
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-11-13 19:58:15 -0500

    mid implement archipelego (still slow)

--- src/puzzle_solver/__init__.py
+++ src/puzzle_solver/__init__.py
@@ -1,5 +1,6 @@
 from puzzle_solver.puzzles.abc_view import abc_view as abc_view_solver
 from puzzle_solver.puzzles.aquarium import aquarium as aquarium_solver
+from puzzle_solver.puzzles.archipelego import archipelego as archipelego_solver
 from puzzle_solver.puzzles.area_51 import area_51 as area_51_solver
 from puzzle_solver.puzzles.arrows import arrows as arrows_solver
 from puzzle_solver.puzzles.battleships import battleships as battleships_solver
@@ -95,6 +96,7 @@ from puzzle_solver.puzzles.inertia.parse_map.parse_map import main as inertia_im
 __all__ = [
     abc_view_solver,
     aquarium_solver,
+    archipelego_solver,
     area_51_solver,
     arrows_solver,
     battleships_solver,
